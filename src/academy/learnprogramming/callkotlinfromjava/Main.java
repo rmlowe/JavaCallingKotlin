efb38412629b66e834b6c3f453022b0345136549
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-19 00:36:02 +0800

    Finished course

--- src/academy/learnprogramming/callkotlinfromjava/Main.java
+++ src/academy/learnprogramming/callkotlinfromjava/Main.java
@@ -1,8 +1,33 @@
 package academy.learnprogramming.callkotlinfromjava;
 
+import academy.learnprogramming.kotlincode.Challenge;
+import academy.learnprogramming.kotlincode.Employee;
+import academy.learnprogramming.kotlincode.KotlinCodeKt;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        KotlinCodeKt.sayHelloToJava("Student");
+
+        Employee employee = new Employee("John", "Smith", 2010);
+        employee.setStartYear(2009);
+
+        Challenge.INSTANCE.doMath(5, 4);
+
+        employee.takesDefault(null);
+
+        //employee.takesDefault("arg1");
+
+//        KotlinStuff.sayHelloToJava("Student");
+//
+//        Employee employee = new Employee("John", "Smith", 2010);
+//        employee.startYear = 2009;
+//
+//        Challenge.doMath(5, 4);
+//
+//        employee.takesDefault("arg1");
+
     }
 }
